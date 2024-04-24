ee6de3e06c8b21d870d646036d495fbe75f030ca
val <35927546+valkraken@users.noreply.github.com>
2024-04-24 15:05:21 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 pip install qtile
 
 cp ./.config/* $HOME/.config/
-cp ./local/bin/* $HOME/.local/bin/
+cp ./local/* $HOME/.local/
 
 # Creating desktop entry
 sudo echo "W0Rlc2t0b3AgRW50cnldCk5hbWU9UXRpbGUKQ29tbWVudD1UaGlzIHNlc3Npb24gbG9ncyB5b3UgaW50byBRdGlsZQpFeGVjPS9ob21lL3BvdGF0by8ubG9jYWwvYmluL3F0aWxlIHN0YXJ0ClR5cGU9QXBwbGljYXRpb24KWC1HRE0tU2Vzc2lvblJlZ2lzdGVycz10cnVlCktleXdvcmRzPXdtO3RpbGluZw==" | base64 --decode > /usr/share/xsessions/qtile.desktop 
