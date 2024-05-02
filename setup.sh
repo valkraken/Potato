0947550f7299b2e7241bce88502af9086d827692
val <35927546+valkraken@users.noreply.github.com>
2024-05-02 22:54:04 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,31 +7,26 @@ if [ $os = 1 ]; then
   sudo apt update && sude apt upgrade -y
   sudo apt install python3-pip rofi zsh git curl
   pip install qtile
+  sudo echo "W0Rlc2t0b3AgRW50cnldCk5hbWU9UXRpbGUKQ29tbWVudD1UaGlzIHNlc3Npb24gbG9ncyB5b3UgaW50byBRdGlsZQpFeGVjPS9ob21lL3BvdGF0by8ubG9jYWwvYmluL3F0aWxlIHN0YXJ0ClR5cGU9QXBwbGljYXRpb24KWC1HRE0tU2Vzc2lvblJlZ2lzdGVycz10cnVlCktleXdvcmRzPXdtO3RpbGluZw==" | base64 --decode > /usr/share/xsessions/qtile.desktop 
+  echo 'export PATH="$HOME/.local/bin:$PATH"' >> $HOME/.bashrc
+  source $HOME/.bashrc
 fi
 
 if [ $os = 2 ]; then
-  sudo pacman -Syu
-  sudo pacman -S rofi zsh git curl nvim python-psutil
+  sudo pacman -Syu -y
+  sudo pacman -S rofi zsh git curl nvim python-psutil -y
   git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
 fi
 
-
-
-echo 'export PATH="$HOME/.local/bin:$PATH"' >> $HOME/.bashrc
-source $HOME/.bashrc
-
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 
 mkdir $HOME/.config
 mkdir -p $HOME/.local/share/fonts
 
-cp ./.config/* $HOME/.config/
-cp ./local/* $HOME/.local/
+cp -r ./.config/* $HOME/.config/
+cp -r ./local/* $HOME/.local/
 
-fc-cache -f -v 
-
-
-# Creating desktop entry
-sudo echo "W0Rlc2t0b3AgRW50cnldCk5hbWU9UXRpbGUKQ29tbWVudD1UaGlzIHNlc3Npb24gbG9ncyB5b3UgaW50byBRdGlsZQpFeGVjPS9ob21lL3BvdGF0by8ubG9jYWwvYmluL3F0aWxlIHN0YXJ0ClR5cGU9QXBwbGljYXRpb24KWC1HRE0tU2Vzc2lvblJlZ2lzdGVycz10cnVlCktleXdvcmRzPXdtO3RpbGluZw==" | base64 --decode > /usr/share/xsessions/qtile.desktop 
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip
+unzip Hack.zip
+cp *.ttf $HOME/.local/share/fonts
+fc-cache -f -v
