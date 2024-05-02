a5dc5f2974ee5e5b2716ceb37edb88b59ba4197e
val <35927546+valkraken@users.noreply.github.com>
2024-05-02 11:48:52 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-sudo apt update && sude apt upgrade -y
+printf "Select arcitecture:\n[1] Ubuntu\n[2] Arch \n> "
+read os
+
+if [ $os = 1 ]; then
+  sudo apt update && sude apt upgrade -y
+  sudo apt install python3-pip rofi zsh git curl
+  pip install qtile
+fi
+
+if [ $os = 2 ]; then
+  sudo pacman -Syu
+  sudo pacman -S python-3 rofi zsh git curl qtile nvim 
+  git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+fi
+
+
 
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> $HOME/.bashrc
 source $HOME/.bashrc
 
-sudo apt install python3-pip rofi zsh git curl
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
-pip install qtile
+mkdir $HOME/.config
+mkdir -p $HOME/.local/share/fonts
 
 cp ./.config/* $HOME/.config/
 cp ./local/* $HOME/.local/
+
+fc-cache -f -v 
+
 
 # Creating desktop entry
 sudo echo "W0Rlc2t0b3AgRW50cnldCk5hbWU9UXRpbGUKQ29tbWVudD1UaGlzIHNlc3Npb24gbG9ncyB5b3UgaW50byBRdGlsZQpFeGVjPS9ob21lL3BvdGF0by8ubG9jYWwvYmluL3F0aWxlIHN0YXJ0ClR5cGU9QXBwbGljYXRpb24KWC1HRE0tU2Vzc2lvblJlZ2lzdGVycz10cnVlCktleXdvcmRzPXdtO3RpbGluZw==" | base64 --decode > /usr/share/xsessions/qtile.desktop 
